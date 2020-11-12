add44959f0a340d8b34ef0147a00a3e63d1dabe4
DavidBak <sejongbgy07@gmail.com>
2020-11-13 03:25:34 +0900

    [Client]/#6/feat: Home(MovieList)페이지 구현

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,16 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MovieListEntry from "./MovieListEntry";
 import Nav from "./Nav";
+import {apiUrl, apiKey, imageBaseUrl} from "./config"
+import axios from "axios"
+import Grid from '@material-ui/core/Grid';
 
-class MovieList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+function MovieList () {
+  const [Movies, setMovies] = useState()
+  
+  
 
-  render() {
-    return <div></div>;
+  useEffect(()=>{
+   const endpoint = `${apiUrl}movie/popular?api_key=${apiKey}&language=ko-KR&page=1`
+   axios.get(endpoint).then(res => {setMovies(res.data.results)
+   }
+   )
+
+  }, [])
+
+  const storage = [];
+  for(let i=0; i<10; i++){
+    storage.push(Movies[i])
   }
-}
+   
+  return (
+    <>
+    {/* <div>{console.log(Movies)}</div> */}
+    <Nav/>
+    <h2>인기 순위</h2>
+    <hr />
+
+    {/* {MovieListEntry 모음} */}
+
+    <Grid container spacing={0} >
+
+      {Movies && storage.map((movie, index) => (
+        <React.Fragment key={index}>
+          <MovieListEntry
+           image={movie.poster_path ? `${imageBaseUrl}w500${movie.poster_path}`: null }
+           movieId={movie.id}
+           movieName={movie.original_title} 
+
+          />
+        </React.Fragment >
+      ))}
+   </Grid>
+    </>
+  )
+ }
+  
+
+  
+ 
+        
 
 export default MovieList;
--- src/components/MovieListEntry.js
+++ src/components/MovieListEntry.js
@@ -1,14 +1,28 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
+import PropTypes from 'prop-types'
 
-class MovieListEntry extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
 
-  render() {
-    return <div></div>;
-  }
+function MovieListEntry(props) {
+ 
+  return( 
+    <Grid item lg= {2} md={4} xs={12}>
+      <div style={{position: 'relative'}}>
+        <a href={`/movie/${props.movieId}`}>
+          <img style={{height:'320px'}} src={props.image} alt={props.movieName}/>
+        </a>
+      </div>
+    </Grid>
+  )
+  
 }
+
+MovieListEntry.propTypes = {
+  movieId:PropTypes.string,
+  image:PropTypes.string,
+  movieName:PropTypes.string,
+}
+
+
 
 export default MovieListEntry;
