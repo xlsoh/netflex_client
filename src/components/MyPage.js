3d88b4da01c48caf7ad95207b4ffbff6f9d84532
yaag19 <m.sun.1290@gmail.com>
2020-11-16 23:32:38 +0900

    modify: handleReviewData()를 componentDidMount()로 수정

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { withRouter, Link } from "react-router-dom";
-import MyReviewList from "./MyReviewList";
-import axios from "axios";
-import PropTypes from "prop-types";
+import React from 'react';
+import { withRouter, Link } from 'react-router-dom';
+import MyReviewList from './MyReviewList';
+import axios from 'axios';
+import PropTypes from 'prop-types';
 
 class MyPage extends React.Component {
   constructor(props) {
@@ -11,14 +11,14 @@ class MyPage extends React.Component {
       myReview: [],
       // myReview:[{reviewId: 1, title:""},{reviewId: 2, title:""}] 배열이다!
     };
-    this.handleReviewData = this.handleReviewData.bind(this);
   }
-  handleReviewData = () => {
+
+  componentDidMount() {
     const { userInfo } = this.props;
     axios
       .get(`http://localhost:5000/movie/reviews/${userInfo.id}`)
       .then((res) => this.setState({ myReview: res.results }));
-  };
+  }
   //res 형식
   //   {
   //     "results": [
@@ -33,27 +33,27 @@ class MyPage extends React.Component {
   //     ]
   // }
   render() {
-    this.handleReviewData();
+    // this.handleReviewData(); // 무한 요청
     const { myReview } = this.state;
     const { isLogin, userInfo, hadleReviewChange } = this.props;
     if (isLogin) {
       return (
-        <div className="myInfoZone">
+        <div className='myInfoZone'>
           <div>
             <h1>내 정보</h1>
             <div>
-              {" "}
-              <span>Email</span>
-              <span>{`${userInfo /*.email 주석을제거해주세요*/}`}</span>
+              {' '}
+              <span>Email </span>
+              <span>{`${userInfo.email}`}</span>
             </div>
             <div>
-              {" "}
-              <span>닉네임</span>
-              <span>{`${userInfo /*.nickName 주석을제거해주세요*/}`}</span>
+              {' '}
+              <span>닉네임 </span>
+              <span>{`${userInfo.nickName}`}</span>
             </div>
           </div>
-          <hr color="black" size="10px"></hr>
-          <div className="myReviewZone">
+          <hr color='black' size='10px'></hr>
+          <div className='myReviewZone'>
             <h2>내가 쓴 리뷰</h2>
             <MyReviewList
               myReview={myReview}
@@ -66,7 +66,7 @@ class MyPage extends React.Component {
       return (
         <div>
           <h1>로그인이 되지 않았습니다</h1>
-          <Link to="/user/signin">로그인 하시겠습니까?</Link>
+          <Link to='/user/signin'>로그인 하시겠습니까?</Link>
         </div>
       );
     }
