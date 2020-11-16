81adcad9e1fc608fbddcdefa2b9fc03b6643d243
박소현 <sohyeon@bagsohyeon-ui-MacBookPro.local>
2020-11-16 11:31:14 +0900

    [Client]/#36/feat: review페이지 좋아요,조회수 구현

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,251 +16,256 @@ class Nav extends React.Component {
   }
 
   render() {
-    const { handleIsLogoutChange } = this.props;
-    return (
-      <Switch>
-        <Route
-          path={`/user/signin`}
-          render={() => {
-            return (
-              <div>
-                <Link to="/user/signin">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    Netflex
-                  </button>
-                </Link>
-              </div>
-            );
-          }}
-        />
-        <Route
-          path={`/user/signup`}
-          render={() => {
-            return (
-              <div>
-                <Link to="/user/signin">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    Netflex
-                  </button>
-                </Link>
-              </div>
-            );
-          }}
-        />
-        <Route
-          path={`/user/mypage`}
-          render={() => {
-            return (
-              <div>
-                <Link to="/movie/popular">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    Netflex
-                  </button>
-                </Link>
-                <Link to="/user/signout" /*서버와 연결시 Link 제거해주세요*/>
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                    onClick={handleIsLogoutChange}
-                  >
-                    로그아웃
-                  </button>
-                </Link>
-              </div>
-            );
-          }}
-        />
-        <Route
-          exact
-          path={`/movie/popular`}
-          render={() => {
-            return (
-              <div>
-                <Link to="/movie/popular">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    Netflex
-                  </button>
-                </Link>
-                <Link to="/user/mypage">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    마이페이지
-                  </button>
-                </Link>
-              </div>
-            );
-          }}
-        />
-        <Route
-          exact
-          path={`/movie/movieId`}
-          render={() => {
-            return (
-              <div>
-                <Link to="/movie/popular">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    Netflex
-                  </button>
-                </Link>
-                <Link to="/user/mypage">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    마이페이지
-                  </button>
-                </Link>
-              </div>
-            );
-          }}
-        />
-        <Route
-          exact
-          path={`/movie/movieId/review/reviewId`}
-          render={() => {
-            return (
-              <div>
-                <Link to="/movie/popular">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    Netflex
-                  </button>
-                </Link>
-                <Link to="/user/mypage">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    마이페이지
-                  </button>
-                </Link>
-              </div>
-            );
-          }}
-        />
-        <Route
-          exact
-          path={`/movie/movieId/writeReview`}
-          render={() => {
-            return (
-              <div>
-                <Link to="/movie/popular">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    Netflex
-                  </button>
-                </Link>
-                <Link to="/user/mypage">
-                  <button
-                    style={{
-                      width: "150px",
-                      height: "50px",
-                      margin: "5px",
-                      borderRadius: "5px",
-                      backgroundColor: "ivory",
-                    }}
-                    type="submit"
-                  >
-                    마이페이지
-                  </button>
-                </Link>
-              </div>
-            );
-          }}
-        />
-      </Switch>
-    );
+    const { isLogin, handleIsLogoutChange } = this.props;
+    if (isLogin) {
+      return (
+        <Switch>
+          <Route
+            path={`/user/signin`}
+            render={() => {
+              return (
+                <div>
+                  <Link to="/user/signin">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      Netflex
+                    </button>
+                  </Link>
+                </div>
+              );
+            }}
+          />
+          <Route
+            path={`/user/signup`}
+            render={() => {
+              return (
+                <div>
+                  <Link to="/user/signin">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      Netflex
+                    </button>
+                  </Link>
+                </div>
+              );
+            }}
+          />
+          <Route
+            path={`/user/mypage`}
+            render={() => {
+              return (
+                <div>
+                  <Link to="/movie/popular">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      Netflex
+                    </button>
+                  </Link>
+                  <Link to="/user/signout" /*서버와 연결시 Link 제거해주세요*/>
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                      onClick={handleIsLogoutChange}
+                    >
+                      로그아웃
+                    </button>
+                  </Link>
+                </div>
+              );
+            }}
+          />
+          <Route
+            exact
+            path={`/movie/popular`}
+            render={() => {
+              return (
+                <div>
+                  <Link to="/movie/popular">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      Netflex
+                    </button>
+                  </Link>
+                  <Link to="/user/mypage">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      마이페이지
+                    </button>
+                  </Link>
+                </div>
+              );
+            }}
+          />
+          <Route
+            exact
+            path={`/movie/movieId`}
+            render={() => {
+              return (
+                <div>
+                  <Link to="/movie/popular">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      Netflex
+                    </button>
+                  </Link>
+                  <Link to="/user/mypage">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      마이페이지
+                    </button>
+                  </Link>
+                </div>
+              );
+            }}
+          />
+          <Route
+            exact
+            path={`/movie/movieId/review/reviewId`}
+            render={() => {
+              return (
+                <div>
+                  <Link to="/movie/popular">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      Netflex
+                    </button>
+                  </Link>
+                  <Link to="/user/mypage">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      마이페이지
+                    </button>
+                  </Link>
+                </div>
+              );
+            }}
+          />
+          <Route
+            exact
+            path={`/movie/movieId/writeReview`}
+            render={() => {
+              return (
+                <div>
+                  <Link to="/movie/popular">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      Netflex
+                    </button>
+                  </Link>
+                  <Link to="/user/mypage">
+                    <button
+                      style={{
+                        width: "150px",
+                        height: "50px",
+                        margin: "5px",
+                        borderRadius: "5px",
+                        backgroundColor: "ivory",
+                      }}
+                      type="submit"
+                    >
+                      마이페이지
+                    </button>
+                  </Link>
+                </div>
+              );
+            }}
+          />
+        </Switch>
+      );
+    } else {
+      return <div></div>;
+    }
   }
 }
 Nav.propTypes = {
   history: PropTypes.object,
+  isLogin: PropTypes.bool,
   handleIsLogoutChange: PropTypes.func,
 };
 export default withRouter(Nav);
