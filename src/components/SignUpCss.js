4499bd6291f1599fe518535f9cc874a649b56d04
DavidBak <sejongbgy07@gmail.com>
2020-11-18 19:11:39 +0900

    [Client]/#55/SignIn페이지 구현하기

---
+++ src/components/SignUpCss.js
@@ -0,0 +1,27 @@
+// import  createGlobalStyle  from 'styled-components';
+// import reset from 'styled-reset'; // css reset
+
+// const GlobalStyleSignUp = createGlobalStyle`
+//     ${reset};
+//     a{
+//         text-decoration: none;
+//         color:inherit;
+//     }
+//     *{
+//         box-sizing:border-box;
+//     }
+//     body{
+//         font-family: 'Nanum Gothic', sans-serif;
+//         font-size: 1rem;
+// 		color:#fff;
+//         width:100%;
+//         height:100vh;
+//         display:flex;
+//         justify-content:center;
+//         align-items:center;
+//         background: url(https://ifh.cc/g/lTFnf7.jpg);
+//         background-size:cover;
+//     }
+// `
+
+// export default GlobalStyleSignUp;
