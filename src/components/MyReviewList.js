69207b9cc9fba5eadeeaf6f001c32ffb4f280dec
DavidBak <65893683+DavidBak@users.noreply.github.com>
2020-11-19 13:02:15 +0900

    Revert "[Client] bug/ MyPage Css 구현 "

--- src/components/MyReviewList.js
+++ src/components/MyReviewList.js
@@ -27,7 +27,6 @@ class MyReviewList extends React.Component {
                   hadleReviewChangeByEdit={hadleReviewChangeByEdit}
                   hadleReviewChangeByTitle={hadleReviewChangeByTitle}
                 />
-                <hr/>
               </React.Fragment>
             );
           })}
--- src/components/MyReviewListEntry.js
+++ src/components/MyReviewListEntry.js
@@ -2,8 +2,6 @@ import React from "react";
 import { withRouter, Link, Route } from "react-router-dom";
 import axios from "axios";
 import PropTypes from "prop-types";
-import "./MovieReviewListEntry.css"
-
 
 class MyReviewListEntry extends React.Component {
   constructor(props) {
@@ -47,11 +45,9 @@ class MyReviewListEntry extends React.Component {
     const { title } = this.props;
     return (
       <div>
-        <a className="title" onClick={this.handleTitleClick}>{title}</a>
-        <span className="btn_group" >
-        <button className="edit" style={{float: "right", padding: "0 20px", border: "1px solid skyblue",margin:"0  0 -1px", background:"white",color:"skyblue", borderRadius:"5px" }} onClick={this.handleEditClick}>수정</button>
-        <button className="delete" style={{float: "right", padding: "0 20px", border: "1px solid skyblue", background:"white", color:"skyblue", borderRadius:"5px"}} onClick={this.handleDelClick}>삭제</button>
-        </span>
+        <a onClick={this.handleTitleClick}>{title}</a>
+        <button onClick={this.handleEditClick}>수정</button>
+        <button onClick={this.handleDelClick}>삭제</button>
       </div>
     );
   }
